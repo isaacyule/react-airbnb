875b3a2d8a951fb02c1639836251c5b695b78d0a
isaac yule <isaac.k.yule@gmail.com>
2018-05-18 15:11:52 +0100

    added £ to prices

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -11,7 +11,7 @@ class Flat extends Component {
     return (
 
       <div className="card" key={this.props.id} style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url(${this.props.info.imageUrl})`}} >
-        <div className="card-category">{this.props.info.price}</div>
+        <div className="card-category">£{this.props.info.price}</div>
         <div className="card-description">
           <h2>{this.props.info.name}</h2>
         </div>
